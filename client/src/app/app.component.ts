02dda4ae458bb0049c346a933fed168a36e8b8dc
Magulaev Marat <magulaevmt@nevz.com>
2020-08-06 10:20:20 +0000

    Yarn config. ToDo list with tasks mock

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Task } from "./task";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Simple ToDo app';
+  // tasks = [];
+  tasks = [
+    new Task(1, 'learn HTML', 'learn HTML fast!'),
+    new Task(2, 'learn CSS', 'learn CSS fast!'),
+    new Task(3, 'learn JavaScript', 'learn JavaScript fast!'),
+    new Task(4, 'learn PHP', 'learn PHP fast!'),
+    new Task(5, 'stay alive!', 'stay alive! fast!'),
+  ];
 }
---
+++ client/src/app/task.ts
@@ -0,0 +1,6 @@
+export class Task {
+    constructor(
+        public id: number,
+        public taskName: string,
+        public taskText: string){}
+}
