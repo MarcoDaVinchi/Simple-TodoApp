f07b8d8ab3e31aa3c26afa7d02233d0b7f553ee7
Marat Magulaev <you@example.com>
2020-09-11 14:20:34 +0000

    Implemented get request from backend

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, map } from 'rxjs/operators';
+import { Todo } from './todo';
 
 const API_URL = environment.apiUrl;
 
@@ -9,4 +12,21 @@ const API_URL = environment.apiUrl;
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
+
+  //API: GET /todos
+  public getAllTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(API_URL + '/todos');
+  }
+
+  //API: POST /todos
+  public createTodo(todo: Todo) {}
+
+  //API: GET /todos/:id
+  public getTodoById(todoId: number) {}
+
+  // API: PUT /todos/:id
+  public updateTodo(todo: Todo) {}
+
+  // API: DELETE /todos/:id
+  public deleteTodoById(todoId: number) {}
 }
--- server/src/todos/todos.controller.ts
+++ server/src/todos/todos.controller.ts
@@ -1,10 +1,29 @@
 import { Controller, Get, Post } from '@nestjs/common';
+import { Todo } from '../todo';
 
+
+const todosMock: Todo[] = [
+  {
+    id: 1,
+    title: 'Read SitePoint article',
+    complete: false,
+  },
+  {
+    id: 2,
+    title: 'Clean inbox',
+    complete: false,
+  },
+  {
+    id: 3,
+    title: 'Make restaurant reservation',
+    complete: false,
+  },
+];
 @Controller('todos')
 export class TodosController {
   @Get()
-  findAll(): string {
-    return 'Returns all Todos.';
+  findAll(): Todo[] {
+    return todosMock;
   }
   @Post()
   create(): string {
