07e9b6e8d1313be6572ce919a452b78f1f9063ec
Marat Magulaev <you@example.com>
2020-09-16 13:34:07 +0000

    added todos route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todos',
+    pathMatch: 'full'
+  },
+  {
+    path: 'todos',
+    component: AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
