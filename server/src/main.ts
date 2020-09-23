079374afc546a4966775d6a3009d1492c7b5d400
Marat Magulaev <you@example.com>
2020-09-23 12:32:24 +0000

    Added helmet & cors config to backend

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  const allowedOrigins = ['http://localhost', 'http://localhost:3000', '*'];
+
+  const cors = {
+    origin: allowedOrigins,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+  };
+
+  app.use(helmet());
+  app.enableCors(cors);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
